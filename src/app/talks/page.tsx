c5d20b6e0c1f579892e1db1418e56feceade9df1
cc <codecolorist@gmail.com>
2025-07-30 01:09:24 +0200

    update talks

--- src/app/talks/page.tsx
+++ src/app/talks/page.tsx
@@ -1,12 +1,40 @@
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
+
 import Talks from "@/markdown/talks.mdx";
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
 
 export default async function Page() {
   return (
-    <div className="font-sans p-8 pb-20 gap-16 sm:p-20">
-      <article className="prose dark:prose-invert">
-        <h1 className="text-3xl font-bold">Talks</h1>
+    <div className="font-sans min-h-screen">
+      <Header />
+
+      <div className="container mx-auto max-md:px-10">
+        <Breadcrumb className="my-6">
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/">Home</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Talks</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+
+      <article className="prose dark:prose-invert mx-auto max-md:px-10">
         <Talks />
       </article>
+
+      <Footer />
     </div>
   );
 }
