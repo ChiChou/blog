6491b00bd842b041371e7504798a554421a8a8a9
cc <codecolorist@gmail.com>
2025-07-30 01:21:07 +0200

    fix asset prefix

--- next.config.ts
+++ next.config.ts
@@ -17,10 +17,12 @@ const ownerAndRepo = process.env.GITHUB_REPOSITORY;
 
 if (typeof ownerAndRepo === "string") {
   const [owner, repo] = ownerAndRepo.split("/", 2);
-  if (repo !== `${owner}.github.io`) {
+  if (repo.toLowerCase() !== `${owner.toLocaleLowerCase()}.github.io`) {
     nextConfig.basePath = `/${repo}`;
   }
 }
+
+console.log("config:", nextConfig);
 
 const withMDX = createMDX({
   extension: /\.(md|mdx)$/,
